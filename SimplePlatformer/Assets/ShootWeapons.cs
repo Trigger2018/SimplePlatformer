187cc1a79377eb11f3f73267fc735c110aaa8833
Travis Kent <travisokent@gmail.com>
2019-04-25 15:33:42 -0600

    upd

---
+++ SimplePlatformer/Assets/ShootWeapons.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootWeapons : MonoBehaviour {
+
+
+	public GameObject barrel;
+	public LaserSiteControll lzrSight;
+	public float speed;
+	public float angle;
+
+
+
+	public UFOController myUFO;
+	public GameObject projectile;
+
+	// Use this for initialization
+	void Start () {
+		myUFO = GetComponent<UFOController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ShootProjectile();
+	}
+
+	public void ShootProjectile()
+	{
+		//Vector3 target = new Vector3(0f,0f,0f);
+		//target=camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, camera.nearClipPlane));
+		/* Vector3 cursorPosition = Input.mousePosition;
+            cursorPosition.z = 2.0f;       // we want 2m away from the camera position
+            this.transform.position = Camera.main.ScreenToWorldPoint(cursorPosition); */
+Debug.Log("object location " +Camera.main.WorldToScreenPoint(transform.position));
+Debug.Log("Mouse location "+Input.mousePosition );
+		Vector3 dir =  Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+		//Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+		angle =   Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		barrel.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+		dir.z =0f;
+		lzrSight.SetEnd(dir);
+		Debug.Log("the mouse coords:" + dir.ToString());
+		Debug.Log("stop point: "+lzrSight.stopPoint);
+		if(Input.GetMouseButtonDown(1))
+		{
+			
+			lzrSight.ToggleLaser();
+		}
+
+
+		if(Input.GetMouseButtonDown(0))
+		{
+			dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+			angle =   Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+			
+			barrel.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+			Instantiate(projectile, transform.position, barrel.transform.rotation );
+		}
+	}
+
+
+
+		
+	
+
+}
--- SimplePlatformer/Assets/UFOController.cs
+++ SimplePlatformer/Assets/UFOController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class UFOController : MonoBehaviour {
 
 	//Add Variables here
-
+	public Rigidbody2D theRB2D;
+	public float movmentSpeed;
+	public float energy;
 
 
 	// Use this for initialization
@@ -15,8 +17,50 @@ public class UFOController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+  
 		//Add Movement Code Here
+		if(Input.GetKeyDown(KeyCode.W))
+		{
+			theRB2D.velocity += new Vector2(0f, 1.0f)*movmentSpeed;
+		}
+		if(Input.GetKeyDown(KeyCode.S))
+		{
+			theRB2D.velocity += new Vector2(0f, -1.0f)*movmentSpeed;
+		}
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			theRB2D.velocity += new Vector2(-1.0f, 0.0f)*movmentSpeed;
+		}
+		if(Input.GetKeyDown(KeyCode.D))
+		{
+			theRB2D.velocity += new Vector2(1.0f, 0f)*movmentSpeed;
+		}
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			energy = energy - 10;
+			theRB2D.velocity = new Vector2(0.0f, 0f)*movmentSpeed;
+		}
 		
+
+		//theRB2D.velocity = new Vector2( Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"))*movmentSpeed;
+
+
+	}
+
+	public void IncreaseSpeed()
+	{
+		movmentSpeed++;
+	}
+	public void DecreaseSpeed()
+	{
+		movmentSpeed--;
+	}
+	public void IncreaseEnergy()
+	{
+		energy++;
+	}
+	public void DecreaseEnergy()
+	{
+		energy--;
 	}
 }
